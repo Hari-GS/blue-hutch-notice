0e50f41ce793f19170a67bb4103bd65c8a318fff
Hari G <sakthihariharan007@gmail.com>
2024-10-23 21:55:05 +0530

    again solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {colors: {
-        customBlue1: '#0088FF',
-        customBlue2: '#002244',
-        milkyGreen: '#98FF98',
-        forGradient: '#1D1FB5'
-
-      }},
-  },
-  plugins: [],
-}
-
-=======
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -29,4 +13,3 @@ export default {
   },
   plugins: [],
 };
->>>>>>> main
